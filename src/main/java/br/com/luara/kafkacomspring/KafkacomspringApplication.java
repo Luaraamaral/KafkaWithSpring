f60fba69d87f15e66108946258fbd570d50220f4
Luara Amaral <98507687+Luaraamaral@users.noreply.github.com>
2023-05-25 17:20:18 -0300

    Commit inicial

---
+++ src/main/java/br/com/luara/kafkacomspring/KafkacomspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.luara.kafkacomspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkacomspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkacomspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luara/kafkacomspring/KafkacomspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luara.kafkacomspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkacomspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
